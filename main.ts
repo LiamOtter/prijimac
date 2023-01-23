0f56da210c565e632fb87e71fbc58d51d0e6cc33
LiamOtter <123409506+LiamOtter@users.noreply.github.com>
2023-01-23 20:23:51 +0100

    Aktualizovať main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -9,24 +9,11 @@ radio.onReceivedNumber(function (receivedNumber) {
         alarm = 0
     }
 })
-input.onButtonPressed(Button.A, function () {
-    radio.sendNumber(input.temperature() + 1000)
-})
-input.onButtonPressed(Button.B, function () {
-    radio.sendNumber(256)
-})
 let ignoruj = 0
 let alarm = 0
 radio.setGroup(1)
 alarm = 0
 ignoruj = 0
-loops.everyInterval(500, function () {
-    if (input.temperature() <= 20) {
-        radio.sendNumber(input.temperature() + 1000)
-    } else {
-        radio.sendNumber(257)
-    }
-})
 loops.everyInterval(500, function () {
     if (alarm && ignoruj == 0) {
         music.startMelody(music.builtInMelody(Melodies.Dadadadum), MelodyOptions.Once)
@@ -36,7 +23,4 @@ loops.everyInterval(60000, function () {
     if (ignoruj > 0) {
         ignoruj += -1
     }
-})
-basic.forever(function () {
-	
 })
