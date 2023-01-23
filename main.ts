7004d0899097dd202df57713fab8d4781b06313b
LiamOtter <123409506+LiamOtter@users.noreply.github.com>
2023-01-23 21:34:54 +0100

    Aktualizovať main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
+/**
+ * 256 stlačilo sa B
+ */
+/**
+ * 257 = teplota OK
+ */
 radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber > 900) {
         basic.showString("" + (receivedNumber - 1000))
@@ -5,8 +11,11 @@ radio.onReceivedNumber(function (receivedNumber) {
     } else if (receivedNumber == 256) {
         alarm = 0
         ignoruj = 5
+        basic.showIcon(IconNames.No)
     } else if (receivedNumber == 257) {
         alarm = 0
+        ignoruj = 0
+        basic.showIcon(IconNames.Yes)
     }
 })
 let ignoruj = 0
@@ -14,9 +23,10 @@ let alarm = 0
 radio.setGroup(1)
 alarm = 0
 ignoruj = 0
-loops.everyInterval(500, function () {
-    if (alarm && ignoruj == 0) {
+loops.everyInterval(10000, function () {
+    if (alarm == 1 && ignoruj == 0) {
         music.startMelody(music.builtInMelody(Melodies.Dadadadum), MelodyOptions.Once)
+        basic.showIcon(IconNames.Sad)
     }
 })
 loops.everyInterval(60000, function () {
